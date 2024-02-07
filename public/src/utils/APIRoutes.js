c1c28d1453c79d12a2517394f5814ec8f0cc6c6b
Himanshu Jikadra <himanshujikadra15@gmail.com>
2024-02-07 11:33:39 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host="http://localhost:5000";
+export const host="https://chatify-nvxy.onrender.com";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
